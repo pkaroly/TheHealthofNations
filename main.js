31b90bc50a98f068ae3ba4a6efb7d0c694706699
Philippa Karoly <philippa.karoly@gmail.com>
2015-08-19 23:37:21 +1000

    edit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ d3.json("nations.json", function(nations) {
 	var canvas = frame.append("g");
 
 	// set plot margins
-	var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5};
+	var margin = {top: 19.5, right: 19.5, bottom: 20.5, left: 39.5};
 	var frame_width = 960;
 	var frame_height = 350;
 	var canvas_width = frame_width - margin.left - margin.right;
@@ -21,6 +21,36 @@ d3.json("nations.json", function(nations) {
 	// Shift the canvas and make it slightly smaller than the svg canvas.
 	canvas.attr("transform", "translate("+ margin.left +","+ margin.top +")");
 
-	var first_circle = canvas.append("circle");
-	first_circle.attr("r", '40px')
+	// var first_circle = canvas.append("circle");
+	// first_circle.attr("r", '40px');
+	// first_circle.attr("fill",'green');
+	// first_circle.attr("stroke",'black');
+	// first_circle.attr("cx",50);
+	// first_circle.attr("cy",30);
+
+	// axes scales
+	var xScale = d3.scale.log()
+	.domain([250, 1e5])
+	.range([0, canvas_width]);
+	var yScale = d3.scale.linear()
+	.domain([10, 85])
+	.range([0, canvas_height]);
+
+	// create the axes
+	var xAxis = d3.svg.axis()
+	.orient("bottom")
+	.scale(xScale);
+	var yAxis = d3.svg.axis()
+	.orient("left")
+	.scale(yScale);
+
+	// draw axes
+	canvas.append("g")
+	.attr("class","x axis")
+	.attr("transform", "translate(0, "+ canvas_height +")")
+	.call(xAxis);
+	canvas.append("g")
+	.attr("class","y axis")
+	.call(yAxis);
+
 });
